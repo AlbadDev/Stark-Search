3ab39f1e2babeb8e700bc9d9278bbbab11aba4ac
Aliou Badara <alioubkeita95@gmail.com>
2021-07-03 04:01:12 +0000

    Fecth data from wikipedia Api and return data in Json

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 
+
+
+// search-Button
+// Fetch Data from wikipedia
+
+const displayWikipediaDta = () => {
+    // input value 
+    let searchString = $('#searchString').val();
+    // console.log(searchString);
+    
+    // wikipedia api url
+    let apiUrl = `https://en.wikipedia.org/w/api.php?action=opensearch&search= ${searchString}  &format=json&callback=Callback`;
+    
+    // Make Ajax request
+    $.ajax({
+        url: apiUrl,
+        dataType: 'jsonp',
+        jsonp: 'callback',
+        success: (res) => {
+            // outPut data searched from wikipedia onto screen
+            console.log(res)
+        }
+    });
+    return false;
+}
+
+$('#form').submit(displayWikipediaDta);
